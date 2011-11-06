d2cf61727789cfd029e20bfc4c86a63d7007771c
Tim Monks <t.r.monks@gmail.com>
2011-11-06 22:08:44 +0000

    merge in new index page and walkthrough style pages from my old repo

---
+++ js/edenpage.js
@@ -0,0 +1,34 @@
+function convertToEdenPage(page) {
+	$('.exec', page).each(function() {
+		var $area = $('<div class="code"></div>');
+		var $previous = $(this).prev();
+		var $code = $(this).clone();
+
+		$(this).remove();
+		$area.insertAfter($previous);
+		var editor;
+		if ($code.hasClass('eden')) {
+			editor = CodeMirror($area.get(0), {
+				value: $code.text(),
+				mode: "eden"
+			});
+		} else {
+			editor = CodeMirror($area.get(0), {
+				value: $code.text(),
+				mode: "javascript"
+			});
+		}
+
+		var $button;
+		if ($code.hasClass('eden')) {
+			$button = $('<button>execute</button>').click(function() {
+				eval(Eden.translateToJavaScript(editor.getValue()));
+			});
+		} else {
+			$button = $('<button>execute</button>').click(function() {
+				eval(editor.getValue());
+			});
+		}
+		$area.append($button);
+	});
+}
