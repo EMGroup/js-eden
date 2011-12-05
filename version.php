2bf919e5dce366b0ce1d4c0d3e0c85685297bdb4
Nicolas Pope <nwpope@gmail.com>
2011-12-05 12:04:42 +0000

    Version detects for DCS special case

--- version.php
+++ version.php
@@ -1,5 +1,8 @@
 <?
 
-system("git describe --abbrev=4 HEAD");
+if (strpos($_SERVER['SERVER_NAME'],".dcs.",0) == FALSE)
+	system("git describe --abbrev=4 HEAD");
+else
+	system("/local/java/git/bin/git describe --abbrev=4 HEAD");
 
 ?>
