8899d0d29f1bccf754bdc5ec9de9b53e2bfb74b8
Matt Cranham <cranman@hotmail.co.uk>
2012-07-10 17:45:58 +0100

    changes to let the jspe copy to the eden interpreter window

--- js/edenpage.js
+++ js/edenpage.js
@@ -34,75 +34,151 @@ function convertToEdenPage(page) {
 }
 
 function convertToEdenPageNew(page,cclass) {
-	var editor;
-	$('.exec', page).each(function() {
-		var $area = $('<div class="'+cclass+'"></div>');
-		var $previous = $(this).prev();
-		var $code = $(this).clone();
+	// Commented out to make the editor global so it's easier for jspe.jse to copy to input window
+	// var editor;
+	if (page == '#eden-input' && cclass == 'code') {
+		$('.exec', page).each(function() {
+			var $area = $('<div class="'+cclass+'"></div>');
+			var $previous = $(this).prev();
+			var $code = $(this).clone();
 
-		$(this).remove();
-		//var editor;
-		$area.insertAfter($previous);
-		if ($code.hasClass('eden')) {
-			editor = CodeMirror($area.get(0), {
-				value: $code.text(),
-				mode: "eden"
+			$(this).remove();
+			//var editor;
+			$area.insertAfter($previous);
+			if ($code.hasClass('eden')) {
+				edeninput = CodeMirror($area.get(0), {
+					value: $code.text(),
+					mode: "eden"
+				});
+			} else {
+				edeninput = CodeMirror($area.get(0), {
+					value: $code.text(),
+					mode: "javascript"
+				});
+			}
+	
+			$area.click(function() {
+				edeninput.focus();
 			});
-		} else {
-			editor = CodeMirror($area.get(0), {
-				value: $code.text(),
-				mode: "javascript"
+	
+			/* DOES NOT WORK */
+			$area.bind('keyup','alt+n', function() {
+				edeninput.setValue(eden.nextHistory());
 			});
-		}
+			$area.bind('keyup','alt+p', function() {
+				edeninput.setValue(eden.previousHistory());
+			});
+	
+			/*var $buttonarea = $('<div class="code_buttons"></div');
+			//$area.append($buttonarea);
+			$buttonarea.insertAfter($area);
+	
+			$buttonarea.append($('<button>Previous</button>').click(function() {
+				edeninput.setValue(eden.previousHistory());
+			}));
 
-		$area.click(function() {
-			editor.focus();
+			$buttonarea.append($('<button>Next</button>').click(function() {
+				edeninput.setValue(eden.nextHistory());
+			}));
+
+			var $button;
+			if ($code.hasClass('eden')) {
+				$button = $('<button>Submit</button>').click(function() {
+
+					try {
+						eden.addHistory(edeninput.getValue());
+						eval(Eden.translateToJavaScript(edeninput.getValue()));
+						edeninput.setValue("");
+						//printSymbolTable();
+						printAllUpdates();
+						//eden.saveLocalModelState();
+					} catch(e) {
+						$('#error-window').addClass('ui-state-error').append("<div class=\"error-item\">## ERROR number " + eden.errornumber + ":<br>\n" + e.message + "</div>\r\n\r\n").dialog({title:"EDEN Errors"});
+						eden.errornumber = eden.errornumber + 1;
+					}
+
+					//eval(Eden.translateToJavaScript(edeninput.getValue()));
+				});
+			} else {
+				$button = $('<button>execute</button>').click(function() {
+					eval(edeninput.getValue());
+				});
+			}
+			$buttonarea.append($button);*/
 		});
+		return edeninput;
+	} else {
+		var editor;
 
-		/* DOES NOT WORK */
-		$area.bind('keyup','alt+n', function() {
-			editor.setValue(eden.nextHistory());
-		});
-		$area.bind('keyup','alt+p', function() {
-			editor.setValue(eden.previousHistory());
-		});
+		$('.exec', page).each(function() {
+                	var $area = $('<div class="'+cclass+'"></div>');
+	                var $previous = $(this).prev();
+	                var $code = $(this).clone();
 
-		/*var $buttonarea = $('<div class="code_buttons"></div');
-		//$area.append($buttonarea);
-		$buttonarea.insertAfter($area);
+        	        $(this).remove();
+                	//var editor;
+	                $area.insertAfter($previous);
+        	        if ($code.hasClass('eden')) {
+                	        editor = CodeMirror($area.get(0), {
+                        	        value: $code.text(),
+                                	mode: "eden"
+	                        });
+        	        } else {
+                	        editor = CodeMirror($area.get(0), {
+                        	        value: $code.text(),
+                                	mode: "javascript"
+	                        });
+        	        }
 
-		$buttonarea.append($('<button>Previous</button>').click(function() {
-			editor.setValue(eden.previousHistory());
-		}));
+                	$area.click(function() {
+                        	editor.focus();
+	                });
 
-		$buttonarea.append($('<button>Next</button>').click(function() {
-			editor.setValue(eden.nextHistory());
-		}));
+        	        /* DOES NOT WORK */
+                	$area.bind('keyup','alt+n', function() {
+                        	editor.setValue(eden.nextHistory());
+	                });
+        	        $area.bind('keyup','alt+p', function() {
+                	        editor.setValue(eden.previousHistory());
+	                });
 
-		var $button;
-		if ($code.hasClass('eden')) {
-			$button = $('<button>Submit</button>').click(function() {
+        	        /*var $buttonarea = $('<div class="code_buttons"></div');
+                	//$area.append($buttonarea);
+	                $buttonarea.insertAfter($area);
 
-				try {
-					eden.addHistory(editor.getValue());
-					eval(Eden.translateToJavaScript(editor.getValue()));
-					editor.setValue("");
-					//printSymbolTable();
-					printAllUpdates();
-					//eden.saveLocalModelState();
-				} catch(e) {
-					$('#error-window').addClass('ui-state-error').append("<div class=\"error-item\">## ERROR number " + eden.errornumber + ":<br>\n" + e.message + "</div>\r\n\r\n").dialog({title:"EDEN Errors"});
-					eden.errornumber = eden.errornumber + 1;
-				}
+        	        $buttonarea.append($('<button>Previous</button>').click(function() {
+                	        editor.setValue(eden.previousHistory());
+	                }));
 
-				//eval(Eden.translateToJavaScript(editor.getValue()));
-			});
-		} else {
-			$button = $('<button>execute</button>').click(function() {
-				eval(editor.getValue());
-			});
-		}
-		$buttonarea.append($button);*/
-	});
-	return editor;
+        	        $buttonarea.append($('<button>Next</button>').click(function() {
+                	        editor.setValue(eden.nextHistory());
+	                }));
+
+	                var $button;
+	                if ($code.hasClass('eden')) {
+	                        $button = $('<button>Submit</button>').click(function() {
+
+        	                        try {
+                	                        eden.addHistory(editor.getValue());
+                        	                eval(Eden.translateToJavaScript(editor.getValue()));
+                                	        editor.setValue("");
+                                        	//printSymbolTable();
+	                                        printAllUpdates();
+        	                                //eden.saveLocalModelState();
+                	                } catch(e) {
+                        	                $('#error-window').addClass('ui-state-error').append("<div class=\"error-item\">## ERROR number " + eden.errornumber + ":<br>\n" + e.message + "</div>\r\n\r\n").dialog({title:"EDEN Errors"});
+	                                        eden.errornumber = eden.errornumber + 1;
+        	                        }
+
+	                                //eval(Eden.translateToJavaScript(editor.getValue()));
+        	                });
+	                } else {
+        	                $button = $('<button>execute</button>').click(function() {
+                	                eval(editor.getValue());
+                        	});
+	                }
+        	        $buttonarea.append($button);*/
+	        });
+        	return editor;
+	}
 }
