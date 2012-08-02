60775fdf9c63df13c1f4c23ea328132bdbf2ce82
Matt Cranham <cranman@hotmail.co.uk>
2012-08-02 13:41:12 +0100

    Adding support for Dracula graph library

--- js/jseden.rb
+++ js/jseden.rb
@@ -8,7 +8,12 @@ $cgi = CGI.new("html4")
 puts "Content-type: text/javascript"
 puts ""
 
-scripts = ["jquery.color.js","jquery.hotkeys.js","dummyconsole.js","json2.js","test.js","maintainer.js","eden/parser.js","eden.js","edenui.js","models.js","collections.js","sessions.js","interpreter.js","em.js","edenpage.js","sylvester.js"]
+# scripts = ["jquery.color.js","jquery.hotkeys.js","dummyconsole.js","json2.js","test.js","maintainer.js","eden/parser.js","eden.js","edenui.js","models.js","collections.js","sessions.js","interpreter.js","em.js","edenpage.js","sylvester.js", "raphael-min.js", "dracula_graffle.js",  "dracula_graph.js", "Curry-1.0.1.js", "dracula_algorithms.js", "dracula_graph.js", "jquery-1.4.2.min.js", "seedrandom.js"]
+
+# scripts = ["jquery.color.js","jquery.hotkeys.js","dummyconsole.js","json2.js","test.js","maintainer.js","eden/parser.js","eden.js","edenui.js","models.js","collections.js","sessions.js","interpreter.js","em.js","edenpage.js","sylvester.js"]
+
+scripts = ["jquery.color.js","jquery.hotkeys.js","dummyconsole.js","json2.js","test.js","maintainer.js","eden/parser.js","eden.js","edenui.js","models.js","collections.js","sessions.js","interpreter.js","em.js","edenpage.js","sylvester.js", "raphael-min.js", "dracula_graffle.js",  "dracula_graph.js"]
+
 
 scripts.each do |x|
 	#puts x
