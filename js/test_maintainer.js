6123a93a1462b0fa04d04e7f30bf5a018f32d3ae
Tim Monks <t.r.monks@gmail.com>
2012-11-11 16:58:12 +0000

    Add failing test to test out travis build.

--- js/test_maintainer.js
+++ js/test_maintainer.js
@@ -1,6 +1,19 @@
 QUnit.module("Folder.prototype.lookup");
 
-test("lookup results in a Symbol", function (assert) {
+test("results in a Symbol", 1, function (assert) {
 	var root = new Folder();
 	notEqual(root.lookup('a').value, undefined);
+});
+
+QUnit.module("Folder.prototype.subscribe");
+
+test("should fail if a circular dependency introduced", 1, function (assert) {
+	var root = new Folder();
+	var A = root.lookup('A'),
+		B = root.lookup('B');
+
+	raises(function () {
+		A.subscribe('B');
+		B.subscribe('A');
+	});
 });
