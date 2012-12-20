23d9f204809de9c19bdf30d8e0a2f212101380c1
Nick Pope <nwpope@gmail.com>
2012-12-20 10:27:49 +0000

    Fixed starts_with? method name error

--- library/libeden.rb
+++ library/libeden.rb
@@ -5,7 +5,7 @@ puts "Content-type: text/plain\n\n"
 def includeScript(script)
 	file = File.new(script, "r")
 	while (line = file.gets)
-		if line.starts_with?("include(")
+		if line.start_with?("include(")
 			comps = line.split("\"")
 			includeScript("../#{comps[1]}")
 		else
