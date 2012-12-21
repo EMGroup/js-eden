81e16f6fffbc42dbb12174c1ab9f8f4cf5bf6167
Nick Pope <nwpope@gmail.com>
2012-12-21 18:53:41 +0000

    Provisional implementation of actually showing symbols

--- plugins/symbol-viewer/symbol-viewer.js
+++ plugins/symbol-viewer/symbol-viewer.js
@@ -10,16 +10,127 @@ Eden.plugins.SymbolViewer = function(context) {
 	/** @public */
 	this.instances = new Array();
 
+	var add_function = function(symresults, symbol, name) {
+
+	}
+
+	var add_observable = function(symresults, symbol, name) {
+		var val = symbol.value();
+		var valhtml;
+		if (typeof val == "boolean") { valhtml = "<span class='special_text'>"+val+"</span>"; }
+		else if (typeof val == "undefined") { valhtml = "<span class='error_text'>undefined</span>"; }
+		else if (typeof val == "string") { valhtml = "<span class='string_text'>\""+val+"\"</span>"; }
+		else if (typeof val == "number") { valhtml = "<span class='numeric_text'>"+val+"</span>"; }
+		else { valhtml = val; }
+
+		var namehtml;
+		if (symbol.definition !== undefined) {
+			namehtml = "<span class=\"hasdef_text\">"+name+"</span>";
+		} else {
+			namehtml = name;
+		}
+
+		var ele = $('<div id="sbobs_' + name + '" class="symbollist-result-element"></div>');
+		ele.html("<li class=\"type-observable\"><span class=\"result_name\">" + namehtml + "</span><span class='result_value'> = " + valhtml + "</span></li>").appendTo(symresults);
+		ele.get(0).symbol = symbol;
+
+		ele.hover(
+			function() {
+				//if (this != selected_observable) {
+				//	$(this).animate({backgroundColor: "#eaeaea"}, 100);
+				//}
+				//var info = $('#observable-info');
+
+				//if (this.symbol.definition !== undefined) {
+				//	var iname = info.find('#observable-info-name');
+				//	iname.text(this.symbol.eden_definition);
+				//	info.css("left", "" + (this.offsetLeft + this.offsetWidth) + "px");
+				//	info.css("top", "" + (this.offsetTop + 125 - 8 - ((info[0].offsetHeight / 2))) + "px");
+				//	info.show();
+				//} else {
+				//	info.hide();
+				//}
+			}, function() {
+				//$('#observable-info').hide();
+				//if (this != selected_observable) {
+				//	$(this).animate({backgroundColor: "white"}, 100);
+				//}
+			}	
+		).click(function() {
+			//if (selected_observable != null) {
+			//	$(selected_observable).animate({backgroundColor: "white"}, 100);
+			//}
+			//selected_observable = this;
+			//$(this).animate({backgroundColor: "#ffebc9"}, 100);
+
+			//this.dialog = observable_dialog(this.symbol, this.dialog);
+		});
+	};
+
+	var add_procedure = function(symresult, symbol, name) {
+		var proc = $('<div class="symbollist-result-element"></div>');
+		proc.html("<li class=\"type-procedure\"><span class=\"result_name\">" + name + "</span></li>").appendTo(symresults);
+
+		proc.get(0).symbol = symbol;
+
+		proc.hover(
+			function() {
+				$(this).animate({backgroundColor: "#eaeaea"}, 100);
+			}, function() {
+				$(this).animate({backgroundColor: "white"}, 100);
+			}	
+			).click(function() {
+		
+			//this.dialog = procedure_dialog(this.symbol, this.dialog);
+			}
+		);
+	};
+
+	var shouldAdd = function(maybeRegex, name) {
+		return !maybeRegex || name.search(maybeRegex) != -1;
+	}
+
 	/** @private */
-	var updateAllViewers = function(pattern) {
+	var initialiseAllViewers = function(pattern) {
 		for (x in me.instances) {
-			updateViewer(me.instances[x],pattern);
+			initialiseViewer(me.instances[x],pattern);
 		}
 	}
 
 	/** @private */
-	var updateViewer = function(element,pattern) {
-		
+	var initialiseViewer = function(element,pattern) {
+		//Clear existing results and start again
+		var symresults = $(element).find(".results-lim > .symbollist-results");
+		symresults.html('');
+
+		var type = element.symboltype;
+		var reg = new RegExp("^"+pattern+".*");
+
+		//For every js-eden symbol
+		$.each(root.symbols, function (name, symbol) {
+			if (!shouldAdd(reg,name)) {
+				return;
+			}
+
+			//Does the symbol have a definition
+			if (!symbol.definition || !symbol.eden_definition) {
+				if ((type == "obs") || (type == "all")) {
+					add_observable(symresults,symbol, name);
+					return;
+				}
+			} else {
+				//Find out what kind of definition it is (proc, func or plain)
+				var subs = symbol.eden_definition.substring(0,4);
+			
+				if (subs == "proc" && (type == "agent" || type == "all")) {
+					add_procedure(symresults,symbol, name);
+				} else if (subs == "func" && (type == "func" || type == "all")) {
+					add_function(symresults,symbol, name);
+				} else if (type == "obs" || type == "all") {
+					add_observable(symresults,symbol, name);
+				}
+			}
+		}
 	}
 
 	/** @private */
@@ -50,9 +161,9 @@ Eden.plugins.SymbolViewer = function(context) {
 
 		me.instances.push(code_entry[0]);
 		code_entry[0].symboltype = type;
-		updateViewer(code_entry[0],"");
+		initialiseViewer(code_entry[0],"");
 		code_entry.find(".search-box-outer > .symbollist-search").keyup(function() {
-			updateViewer(code_entry[0],this.value);
+			initialiseViewer(code_entry[0],this.value);
 		});
 	}
 
@@ -83,4 +194,4 @@ Eden.plugins.SymbolViewer = function(context) {
 /* Plugin meta information */
 Eden.plugins.SymbolViewer.title = "Symbol Viewer";
 Eden.plugins.SymbolViewer.description = "Provide various views of the symbol table";
-Eden.plugins.SymbolViewer.author = "Nicolas Pope";
+Eden.plugins.SymbolViewer.author = "Nicolas Pope and Tim Monks";
