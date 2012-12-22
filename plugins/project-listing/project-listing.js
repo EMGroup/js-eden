7c2632beefec860a4f76d9f69097048331199d2e
Nick Pope <nwpope@gmail.com>
2012-12-22 13:43:05 +0000

    Search and config icon for project list

--- plugins/project-listing/project-listing.js
+++ plugins/project-listing/project-listing.js
@@ -100,9 +100,9 @@ Eden.plugins.ProjectList = function(context) {
 
 	/** @private */
 	var generateHTML = function() {
-		return "<div class=\"projectlist-search-box-outer\">\
+		return "<div class=\"projectlist-search-icon\"></div><div class=\"projectlist-search-box-outer\">\
 			<input type=\"text\" class=\"projectlist-search search-box\"></input>\
-		</div>\
+		</div><div class=\"projectlist-config-icon\"></div>\
 		<div class=\"projectlist-results\"></div>";
 	}
 
