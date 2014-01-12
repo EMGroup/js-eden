4e7621dd1aca0d85c2d3d60fbb203715c7cbf28b
RuthRainbow <ruth.king1991@gmail.com>
2014-01-12 21:56:33 +0000

    renaming entities -> definitions, agents -> entities

--- plugins/adm/adm-input.js
+++ plugins/adm/adm-input.js
@@ -10,11 +10,11 @@
 	
 	this.actionsList;
 	
-	this.agents = new Array();
+	this.entities = new Array();
 	// Definition store: (probably this isn't needed)
 	this.definitions = new Array();
 	
-	// Holds the index of the agent currently displayed in the input box.
+	// Holds the index of the entity currently displayed in the input box.
 	this.currIndex = -1;
 	
 	// Holds guard / action sequence pairs.
@@ -27,10 +27,10 @@
 		return this.guard + ' --> ' + this.action;
 	};
 
-	// Holds information defining an agent:
-	function Agent(name, entities, actionsArr) {
+	// Holds information defining an entity:
+	function Entity(name, definitions, actionsArr) {
 		this.name = name;
-		this.entities = entities;
+		this.definitions = definitions;
 		this.actionsArr = actionsArr;
 	};
 	
@@ -39,32 +39,32 @@
 		return '<div id="'+name+'-input" class=\"inputwindow-code\">\
 			<form>\
 				<label>Name: </label><input id="adm-name" type=\"text\" class=\"adm-name\"></input><br>\
-				<label>Entities:</label><br><textarea id="adm-entities" class=\"adm-entities\"></textarea><br>\
+				<label>Definitions:</label><br><textarea id="adm-definitions" class=\"adm-definitions\"></textarea><br>\
 				<label>Actions:</label><br><textarea id="adm-actions" class=\"adm-actions\"></textarea>\
 			</form>\
 		</div>\
-		<div id="adm-results" class=\"agentlist-results\">\
+		<div id="adm-results" class=\"entitylist-results\">\
 			<ul id="results"> </ul>\
 		</div>';
 	};
 	
-	/* Process the entities of a newly created agent. */
-	var processEntities = function(entities) {
-		for (var i = 0; i < entities.length; i++) {
-			// Submit each entitiy as eden code to add to definition store.
+	/* Process the definitions of a newly created entity. */
+	var processDefinitions = function(definitions) {
+		for (var i = 0; i < definitions.length; i++) {
+			// Submit each definition as eden code to add to definition store.
 			try {
-				// TODO append name of agent instance to entity.
-				eval(Eden.translateToJavaScript(entities[i]));
+				// TODO append name of entity instance to definition.
+				eval(Eden.translateToJavaScript(definitions[i]));
 			} catch (e) {
 				Eden.reportError(e);
 				return -1;
 			}
-			me.definitions.push(entities[i]);
+			me.definitions.push(definitions[i]);
 		}
 	};
 	
-	/* Process the actions of a newly created agent. */
-	var processActions = function(name, entities, actions) {
+	/* Process the actions of a newly created entity. */
+	var processActions = function(name, definitions, actions) {
 		// TODO validate here! e.g. by trying to convert to eden code
 		var actionsArr = new Array();
 		for (var i = 0; i < actions.length; i++) {
@@ -74,50 +74,50 @@
 				actionsArr.push(new GuardActions(split[0], split[1]));
 			}
 		}
-		me.agents.push(new Agent(name, entities, actionsArr));
+		me.entities.push(new Entity(name, definitions, actionsArr));
 	};
 	
-	var processNewAgent = function(name) {
-		// TODO add ability to edit existing agent!
-		// TODO append greek letters to agent instances.
+	var processNewEntity = function(name) {
+		// TODO add ability to edit existing entity!
+		// TODO append greek letters to entity instances.
 		// Entities and actions should be separated by \n.
-		var input = document.getElementById('adm-entities'),
-		entities = input.value.split('\n');
+		var input = document.getElementById('adm-definitions'),
+		definitions = input.value.split('\n');
 		var input = document.getElementById('adm-actions'),
 		actions = input.value.split('\n');
 		
-		var returnCode = processEntities(entities);
-		// If entities processed ok, also process actions!
+		var returnCode = processDefinitions(definitions);
+		// If definitions processed ok, also process actions!
 		if (returnCode != -1) {
-			processActions(name, entities, actions);
+			processActions(name, definitions, actions);
 		}
 		return returnCode;
 	};
 	
 	var validateInput = function() {
 		var input = document.getElementById('adm-name'),
-		agentName = input.value;
-		if (agentName) {
-			var returnCode = processNewAgent(agentName);
+		entityName = input.value;
+		if (entityName) {
+			var returnCode = processNewEntity(entityName);
 			
 			if (returnCode != -1) {
-				alert('Agent was successfully added!');
+				alert('Entity was successfully added!');
 				document.getElementById('adm-name').value = '';
-				document.getElementById('adm-entities').value = '';
+				document.getElementById('adm-definitions').value = '';
 				document.getElementById('adm-actions').value = '';
 				
-				// Add a new element in the agent list for this new agent:
+				// Add a new element in the entity list for this new entity:
 				var newdiv = document.createElement('li');
-				newdiv.setAttribute('id',agentName);
-				newdiv.class = 'agentlist-element';
-				newdiv.innerHTML = '<label>'+agentName+'</label>';
+				newdiv.setAttribute('id',entityName);
+				newdiv.class = 'entitylist-element';
+				newdiv.innerHTML = '<label>'+entityName+'</label>';
 				var results = document.getElementById('results');
 				results.appendChild(newdiv);
 				
 				this.currIndex = -1;
 			}
 		} else {
-			alert('Please enter a name for this agent.');
+			alert('Please enter a name for this entity.');
 			input.focus();
 		}
 	};
@@ -128,14 +128,14 @@
 		
 		// List to store the actions we are processing this round.
 		var actions = new Array();
-		alert('processing ' + me.agents.length + ' agents');
+		alert('processing ' + me.entities.length + ' entities');
 		
 		// Find all actions to evaluate and add to the actions list:
-		for (x in me.agents) {
-			var agent = me.agents[x];
-			for (var j = 0; j < agent.actionsArr.length; j++) {
+		for (x in me.entities) {
+			var entity = me.entities[x];
+			for (var j = 0; j < entity.actionsArr.length; j++) {
 				// The guard should be EDEN code! Execute it
-				var action = agent.actionsArr[j];
+				var action = entity.actionsArr[j];
 				try {
 					var answer = eval(Eden.translateToJavaScript('return ' + action.guard + ';'));
 				} catch (e) {
@@ -152,31 +152,31 @@
 		}
 	};
 	
-	/* Display information about the agent at the current index. */
+	/* Display information about the entity at the current index. */
 	var display = function(index) {
 		if (index < -1) {
-			index = me.agents.length - 1;
-		} else if (index >= me.agents.length) {
+			index = me.entities.length - 1;
+		} else if (index >= me.entities.length) {
 			index = -1;
 		}
 		
 		if (index == -1) {
 			document.getElementById('adm-name').value = '';
-			document.getElementById('adm-entities').value = '';
+			document.getElementById('adm-definitions').value = '';
 			document.getElementById('adm-actions').value = '';
 		} else {
-			var agent = me.agents[index];
-			document.getElementById('adm-name').value = agent.name;
-			document.getElementById('adm-entities').value = agent.entities;
-			document.getElementById('adm-actions').value = agent.actionsArr;
+			var entity = me.entities[index];
+			document.getElementById('adm-name').value = entity.name;
+			document.getElementById('adm-definitions').value = entity.definitions;
+			document.getElementById('adm-actions').value = entity.actionsArr;
 		}
 		me.currIndex = index;
 	};
 	
-	var deleteAgent = function(index) {
+	var deleteEntity = function(index) {
 		if (index > -1) {
-			alert('deleting an instance of agent ' + me.agents[index].name);
-			me.agents.splice(index);
+			alert('deleting an instance of entity ' + me.entities[index].name);
+			me.entities.splice(index);
 			me.currIndex = index-1;
 			display(me.currIndex);
 		}
@@ -206,16 +206,16 @@
 					},
 					{
 						id: "btn-add",
-						text: "Add Agent",
+						text: "Add Entity",
 						click: function() {
 							validateInput();
 						}
 					},
 					{
 						id: "btn-delete",
-						text: "Delete Agent",
+						text: "Delete Entity",
 						click: function() {
-							deleteAgent(me.currIndex);
+							deleteEntity(me.currIndex);
 						}
 					},
 					{
@@ -289,7 +289,7 @@ Eden.plugins.ADM.ActionsList.prototype.clear = function() {
 
 Eden.plugins.ADM.Action = function(action) {
 	this.action = action;
-	this.element = $('<div class="agentlist-element"></div>');
+	this.element = $('<div class="entitylist-element"></div>');
 	this.update = this.updateAction;
 
 	this.element.hover(
