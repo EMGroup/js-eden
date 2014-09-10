25d254ccfaaae62795ccd1c9a065bf2098569c15
Tim Monks <t.r.monks@gmail.com>
2014-09-10 14:13:56 +0000

    Don't deploy from travis for PR.

--- travis.sh
+++ travis.sh
@@ -2,6 +2,19 @@
 
 set -e
 
+# don't deploy for PRs
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]
+then
+	exit 0
+fi
+
+# don't deploy for branches other than master
+if [ "$TRAVIS_BRANCH" != "master" ]
+then
+	exit 0
+fi
+
+
 git config --global user.email "bot@travis-ci.org"
 git config --global user.name "travis ci auto commit"
 git config --global credential.helper "store --file=${PWD}/.git/credentials"
