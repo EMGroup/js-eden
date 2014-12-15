66cb79dddc0262789124da1861a388621753e575
Tim Monks <t.r.monks@gmail.com>
2014-12-14 21:54:29 -0800

    Fix writeln.

--- selenium-tests/test.js
+++ selenium-tests/test.js
@@ -18,7 +18,7 @@ describe("UI tests", function() {
 		return browser.init({browserName: 'firefox'});
 	});
 
-	beforeEach(function() {
+	before(function() {
 		return browser.get("http://localhost:8000");
 	});
 
@@ -32,5 +32,32 @@ describe("UI tests", function() {
 
 	it("has a menu", function () {
 		return browser.waitForElementByCss('#menubar-main');
+	});
+
+	it("has an input window", function () {
+		return browser.waitForElementByCss('#inputwindow-dialog');
+	});
+
+	it("describe it has a canvas window", function () {
+		return browser.waitForElementByCss('#default-dialog');
+	});
+
+	it("describe it has a projects window", function () {
+		return browser.waitForElementByCss('#projects-dialog');
+	});
+
+	describe("writeln", function () {
+		before(function () {
+			browser
+			.waitForElementByCss('#inputwindow-dialog textarea')
+			.type('writeln("Hello world!");')
+			.then(function () {
+				return browser.waitForElementByCss('#inputwindow-dialog .submitButton').click();
+			});
+		});
+
+		it("shows in the menu status", function () {
+			return browser.waitForElementByCss('#menubar-status', wd.asserters.textInclude('Hello world!'));
+		});
 	});
 });
