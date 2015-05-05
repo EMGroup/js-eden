aee47fc9e14215c87f57942d3ccc3afeac74205c
Jonathan Foss <github@jonathanfoss.co.uk>
2015-05-05 10:47:01 +0100

    Improved format and added command line options

--- plugins/network-remote/convertLogFormat.sh
+++ plugins/network-remote/convertLogFormat.sh
@@ -1,1 +1,1 @@
-sed "s/Adding \([A-Za-z0-9]*==\) to session \([a-z]*\) (num: \([0-9]*\))/REPLAY:C:\3:\2:\1/" $1 | sed "s/Deleting \([A-Za-z0-9]*==\) from session \([A-Za-z0-9]*\)/REPLAY:D:?:\2:\1/"
+sed "s/Adding \([A-Za-z0-9]*==\) to session \([a-z]*\) (num: \([0-9]*\))/REPLAY:\3:\2:C:\1/" $1 | sed "s/Deleting \([A-Za-z0-9]*==\) from session \([A-Za-z0-9]*\)/REPLAY:?:\2:D:\1/"
--- plugins/network-remote/network-remote-parser.js
+++ plugins/network-remote/network-remote-parser.js
@@ -2,37 +2,69 @@ var filename;
 var fs = require("fs");
 var reqSession;
 var WebSocket = require("ws");
-
 var codeLines;
+var host = "127.0.0.1";
+var port = "8001";
+var debug = false;
+var send = true;
+
+function printUsage(){
+	console.log("Usage:\nnode network-remote-parser.js FILENAME SESSION [--host=HOST] [--port=PORT] [--debug] [--no-send]");
+}
+
 process.argv.forEach(function(val, index, array){
-	if(index == 2)
+	if(index == 2){
 		filename = val;
-	if(index == 3)
+	}else if(index == 3){
 		reqSession = val;
+	}else{
+		if(val == "--help"){
+			printUsage();
+			return;
+		}
+		if(val == "--debug")
+			debug = true;
+		if(val == "--no-send")
+			send = false;
+		var options = val.split("=");
+		if(options[0] == "--port")
+			port = options[1];
+		if(options[0] == "--host")
+			host = options[1];
+	}
 });
+if(reqSession === undefined || filename === undefined){
+	printUsage();
+	return;
+}
 fs.readFile(filename, "utf-8",function(err,data){
 	if(err)
 		return console.log(err);
-	var lines = data.split(/REPLAY:|\nDeleting|\nAdding/);
+	var lines = data.split(/REPLAY:/);
 	var firstTime = -1;
 	codeLines = [];
-	for(var i = 0; i < lines.length;i++){
-		var tokens = lines[i].split(":")
+	for(var i = 1; i < lines.length;i++){
+		var tokens = lines[i].split(":");
 		var session = tokens[1];
 		if(session != reqSession)
 			continue;
 		var sender = tokens[0];
 		var timestamp = tokens[2];
+		if(timestamp == "C" || timestamp == "D")
+			continue;
 		var line = tokens.slice(3).join(":");
 		if(firstTime == -1)
 			firstTime = timestamp;
 		codeLines.push({time:timestamp - firstTime,code:line});
 		firstTime = timestamp;
 	}
-	playback();
+	if(debug)
+		console.log(codeLines);
+	if(send)
+		playback();
 });
 function playback(){
-	var ws = new WebSocket("ws://localhost:8001");
+	var ws = new WebSocket("ws://"+host+":" + port);
 	ws.on('open',function(){
 		ws.send(reqSession);
 		playLine(ws,0);	
