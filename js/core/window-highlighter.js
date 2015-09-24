ef522260e49aeefea64ed9b11335f738820a7a81
Elizabeth Hudnott <lizzy.hudnott@gmail.com>
2015-09-25 00:05:37 +0300

    Fix regressions (multicoloured lines & hidden windows).

--- js/core/window-highlighter.js
+++ js/core/window-highlighter.js
@@ -2,26 +2,35 @@ function WindowHighlighter(edenUI) {
 	this.edenUI = edenUI;
 	this.lastDialog = undefined;
 	this.lastDialogMinimized = false;
+	this.lastDialogHidden = false;
 	this.previousZIndex = undefined;  //z-index before raising, or undefined if window has become unpinned since.
 }
 
 WindowHighlighter.prototype.highlight = function (dialogName) {
 	this.lastDialog = edenUI.getDialogWindow(dialogName);
 	var dialogContent = edenUI.getDialogContent(dialogName);
-	var lastDialogMin = dialogContent.data('dialog-extend-minimize-controls');
-	if (lastDialogMin) {
-		this.previousZIndex = lastDialogMin.css('z-index');
-		dialogContent.dialogExtend("restore");
-		this.lastDialog.removeClass("window-activated");
-		this.lastDialog.addClass('menubar-window-raise');
-		this.lastDialog.css('z-index', 2147483646);
-		this.lastDialogMinimized = true;
+	if (!dialogContent.dialog("isOpen")) {
+		this.previousZIndex = undefined;
+		edenUI.showView(dialogName);
+		edenUI.moveView(dialogName);
+		this.lastDialogHidden = true;
 	} else {
-		this.lastDialog.addClass('menubar-window-raise');
-		this.previousZIndex = this.lastDialog.css('z-index');
-		this.lastDialog.css('z-index', 2147483646);
-		this.lastDialogMinimized = false;
+		var lastDialogMin = dialogContent.data('dialog-extend-minimize-controls');
+		if (lastDialogMin) {
+			this.previousZIndex = lastDialogMin.css('z-index');
+			dialogContent.dialogExtend("restore");
+			this.lastDialog.removeClass("window-activated");
+			this.lastDialogMinimized = true;
+		} else {
+			this.lastDialog.addClass('menubar-window-raise');
+			this.previousZIndex = this.lastDialog.css('z-index');
+			this.lastDialog.css('z-index', 2147483646);
+			this.lastDialogMinimized = false;
+		}
+		this.lastDialogHidden = false;
 	}
+	this.lastDialog.addClass('menubar-window-raise');
+	this.lastDialog.css('z-index', 2147483646);
 };
 
 WindowHighlighter.prototype.stopHighlight = function (dialogName, unminimize) {
@@ -30,8 +39,12 @@ WindowHighlighter.prototype.stopHighlight = function (dialogName, unminimize) {
 	this.lastDialog.removeClass('menubar-window-raise');
 
 	var dialogContent = edenUI.getDialogContent(dialogName);
-	if (this.lastDialogMinimized && !unminimize) {
-		dialogContent.dialogExtend("minimize");
+	if (!unminimize) {
+		if (this.lastDialogHidden) {
+			edenUI.hideView(dialogName);
+		} else if (this.lastDialogMinimized) {
+			dialogContent.dialogExtend("minimize");
+		}
 	}
 
 	if (this.previousZIndex !== undefined) {
@@ -40,6 +53,7 @@ WindowHighlighter.prototype.stopHighlight = function (dialogName, unminimize) {
 
 	this.lastDialog = undefined;
 	this.lastDialogMinimized = false;
+	this.lastDialogHidden = false;
 	this.previousZIndex = undefined;
 };
 
