175cff5d83622a1d357c3c7d310857a88e025c5c
Nicolas Pope <nwpope@gmail.com>
2015-11-30 14:34:33 +0000

    Updated page plugin to support unstructured html and styling. Worksheet template provided as jseden script. Support for jseden tag attributes and jseden-canvas tag

--- plugins/page/page.js
+++ plugins/page/page.js
@@ -14,8 +14,9 @@
 EdenUI.plugins.Page = function(edenUI, success) {
 	var me = this;
 	var thispage = edenUI.eden.root.lookup("page");
+	var thisnpage = edenUI.eden.root.lookup("document");
 	var pagestruct = thispage.value();
-	var pagediv = $("<div class='main-page'></div>");
+	var pagediv = $("<div class='main-page page'></div>");
 	var theme;
 	$("body").append(pagediv).addClass("page-body");
 
@@ -78,6 +79,227 @@ EdenUI.plugins.Page = function(edenUI, success) {
 		}
 	}
 
+
+
+	var stylemapping = [
+		"top",
+		"bottom",
+		"left",
+		"right",
+		"width",
+		"height",
+		"background",
+		"position",
+		"color",
+		"border",
+		"font",
+		"fontSize",
+		"fontFamily",
+		"fontWeight",
+		"borderBottom",
+		"borderLeft",
+		"borderRight",
+		"borderTop",
+		"overflow",
+		"padding",
+		"paddingTop",
+		"paddingLeft",
+		"paddingRight",
+		"paddingBottom",
+		"margin",
+		"marginLeft",
+		"marginRight",
+		"marginTop",
+		"marginBottom",
+		"textShadow"
+	];
+
+
+
+	function cssValue(value) {
+		var type = typeof value;
+
+		switch(type) {
+		case "string"		: return value;
+		case "number"		: return ""+value+"px";
+		default				: return "";
+		}
+	}
+
+
+
+	function generateStyle(e, s) {
+		for (var i=0; i<s.length; i++) {
+			if (s[i] !== undefined && stylemapping[i]) {
+				//console.log("Styling: " + stylemapping[i] + " = " + cssValue(s[i]));
+				e.style[stylemapping[i]] = cssValue(s[i]);
+			}
+		}
+	}
+
+
+	function parseAttributes(text) {
+		var attribs = {};
+		if (text === undefined || text == "") return attribs;
+
+		var components = text.split(" ");
+		var start = 0;
+		if (components[0].charAt(0) == "-") {
+			start = 1;
+			attribs.tagType = components[0].substr(1);
+		}
+
+		for (var i=start; i<components.length; i++) {
+			var parts = components[i].split("=");
+			if (parts.length > 1) {
+				if (parts[1].charAt(0) == "\"") {
+					parts[1] = parts[1].slice(1,-1);
+				} else {
+					parts[1] = parseInt(parts[1]);
+				}
+				attribs[parts[0]] = parts[1];
+			} else {
+				attribs[parts[0]] = true;
+			}
+		}
+
+		return attribs;
+	}
+
+
+
+	function processHTML(text) {
+		var scripts = [];
+		var attribs = [];
+
+		// Do a find replace for the jseden tag
+		text = text.replace(/<jseden([\s\S]*?)>([\s\S]*?)<\/jseden[\S]*>/g,
+		function (match, attributes, code, offset, string) {
+			//console.log("ATTRIBS: " + attributes);
+			scripts.push(code);
+			attribs.push(parseAttributes(attributes));
+			//console.log(parseAttributes(attributes));
+			return "$$$$";
+		});
+
+		var splittext = text.split("$$$$");
+
+		if (splittext.length == 1) {
+			return text;
+		} else {
+			var outer = $("<div></div>");
+			for (var i=0; i<splittext.length-1; i++) {
+				outer.append($("<div class='page-paragraph'>"+splittext[i]+"</div>"));
+
+				// Plain jseden tag, backwards compat with JSPE
+				if (attribs[i] === undefined || attribs[i].tagType === undefined) {
+					var linecount = scripts[i].split("\n").length;
+					//var script = generateScript(["script", false, [scripts[i]], undefined, true, linecount, false, false, "50%"]);
+					if (attribs[i].lines === undefined) attribs[i].lines = linecount;
+					if (attribs[i].width === undefined) attribs[i].width = "50%";
+					if (attribs[i].box === undefined) attribs[i].box = true;
+					var script = generateScript(["script", attribs[i]["static"],[scripts[i]],attribs[i].name,attribs[i].box,attribs[i].lines,false,attribs[i].float,attribs[i].width]);
+					outer.append(script);
+				} else if (attribs[i]) {
+					console.log("Some other view: " + attribs[i].tagType);
+					if (attribs[i].tagType == "canvas") {
+						var can = generateCanvas(["canvas", attribs[i].name,attribs[i].source,attribs[i].width,attribs[i].height,attribs[i].float,attribs[i].box])
+						outer.append(can);
+					}
+				}
+			}
+			outer.append($("<div class='page-paragraph'>"+splittext[splittext.length-1]+"</div>"));
+			return outer.get(0);
+		}
+	}
+
+
+
+	function generateElement(p) {
+		if (!p) return;
+		if (p[0] != "element") return;
+
+		var type = p[1];
+		var id = p[2];
+		var classname = p[3];
+		var content = p[4];
+		var style = p[5];
+		var clickable = p[6];
+		var mouseenter = p[7];
+		var mouseleave = p[8];
+		var mousedown = p[9];
+		var mouseup = p[10];
+		var mousemove = p[11];
+		var src = p[12];
+		var href = p[13];
+
+		console.log("Element: " + type);
+
+		var ele;
+		switch (type) {
+		case "div":
+		case "span":
+		case "button":
+		case "img":
+		case "p":
+		case "ul":
+		case "ol":
+		case "li":
+		case "br":
+		case "pre":
+		case "h1":
+		case "h2":
+		case "h3":
+		case "a":
+		case "input":	ele = document.createElement(type); break;
+		default: 	console.error("Invalid element type: " + type);
+					return;
+		}
+
+		if (id) ele.id = id;
+		if (classname) ele.className = classname;
+		if (content && typeof content == "string") {
+			var processed = processHTML(content);
+			if (typeof processed == "string") {
+				ele.innerHTML = content;
+			} else {
+				ele.appendChild(processed);
+			}
+		}
+		if (content && content instanceof Array) generateContentList(ele, content);
+		if (style && typeof style == "string") ele.cssText = style;
+		if (style && style instanceof Array) generateStyle(ele, style);
+		if (clickable && id) ele.onclick = function(e) {
+			var sym = eden.root.lookup(id+"_click");
+			sym.assign(true, eden.root.scope);
+			sym.assign(false, eden.root.scope);
+		}
+		if (type == "img" && src) ele.src = src;
+		if (type == "a" && href) ele.href = href;
+
+		return ele;
+	}
+
+
+
+	function generateContentList(ele, content) {
+		if (typeof content[0] == "string") {
+			var child = generateElement(content);
+			if (child) {
+				ele.appendChild(child);
+			}
+			return;
+		}
+		for (var i=0; i<content.length; i++) {
+			var child = generateElement(content[i]);
+			if (child) {
+				ele.appendChild(child);
+			}
+		}
+	}
+
+
+
 	function generateParagraph(p) {
 		if (!p) return "";
 		if (p[0] != "p") return "";
@@ -134,7 +356,7 @@ EdenUI.plugins.Page = function(edenUI, success) {
 			scriptnum++;
 		}
 
-		if (isStatic == false) {
+		if (!isStatic) {
 			var embedded;
 			if (scripts[name]) {
 				embedded = scripts[name];
@@ -215,6 +437,11 @@ EdenUI.plugins.Page = function(edenUI, success) {
 			container = $("<div class='page-canvas'></div>");
 		}
 
+		console.log("CREATE CANVAS: " + content[1] + " width=" + content[3] + ", height=" + content[4]);
+
+		if (width) container.css("width", cssValue(width));
+		if (height) container.css("height", cssValue(height));
+
 		if (canvases[content[1]] === undefined) {
 			var embedded = edenUI.views["Canvas2D"].embedded(content[1],content[1],content[2]);
 			canvases[content[1]] = embedded;
@@ -271,7 +498,20 @@ EdenUI.plugins.Page = function(edenUI, success) {
 		return res;
 	}
 
+
+
+	function generateDOM(symbol, value) {
+		if (value === undefined) return;
+		if (!(value instanceof Array)) return;
+
+		pagediv.html("");
+		generateContentList(pagediv.get(0), value);
+	}
+
+
+
 	function generatePage(symbol, value) {
+		if (value === undefined) return;
 		if (!(value instanceof Array)) return;
 
 		scriptnum = 0;
@@ -307,6 +547,7 @@ EdenUI.plugins.Page = function(edenUI, success) {
 	generatePage(thispage, pagestruct);
 
 	thispage.addJSObserver("pageGenerator", generatePage);
+	thisnpage.addJSObserver("pageGenerator", generateDOM);
 
 	//Note this plugin does not have a view
 
