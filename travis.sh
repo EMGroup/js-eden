ec3ba8a90b9848d1ea5fe47cf7769ab90a3faea1
Nicolas Pope <nwpope@gmail.com>
2015-12-09 23:15:35 +0000

    Another travis fix?

--- travis.sh
+++ travis.sh
@@ -26,14 +26,14 @@ then
 	exit 0
 fi
 
-
+git describe --tags HEAD
 git config --global user.email "bot@travis-ci.org"
 git config --global user.name "travis ci auto commit"
 git config --global credential.helper "store --file=${PWD}/.git/credentials"
 git remote rm origin
 git remote add origin "https://github.com/EMGroup/js-eden.git"
 echo "https://${GH_TOKEN}:@github.com" > .git/credentials
-git tag
+git describe --tags HEAD
 node write_version.js
 grunt gh-pages
 echo "Deployed js-eden!"
