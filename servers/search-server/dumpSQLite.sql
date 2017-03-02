0b9b89d0b485e06c5f053792f87a88b31cd64a5b
Jonathan Foss <github@jonathanfoss.co.uk>
2017-03-02 14:39:39 +0000

    Database modifications to match live server version

--- servers/search-server/dumpSQLite.sql
+++ servers/search-server/dumpSQLite.sql
@@ -58,9 +58,10 @@ CREATE TABLE comments(
 	"public" INTEGER NOT NULL,
 	"comment" TEXT NOT NULL
 );
+
 CREATE UNIQUE INDEX "commentID" on comments(commentID ASC);
-CREATE INDEX "comments_projectID" on comments(projectID ASC);
 CREATE INDEX "comments_date" on comments(date ASC);
+CREATE INDEX "comments_projectID" on comments(projectID ASC);
 
 CREATE VIEW view_latestVersion AS
 select saveID,date,projectid FROM (SELECT max(saveID) as maxsaveID from projectversions 
@@ -70,18 +71,20 @@ CREATE VIEW view_listedVersion AS
 select saveID,date,projects.projectid FROM projectversions,projects where saveID = publicVersion;
 
 
+CREATE UNIQUE INDEX "oauthstring" on oauthusers (oauthstring ASC);
+CREATE INDEX "versions_projectID" on projectversions (projectID ASC);
+
+CREATE UNIQUE INDEX "projectID" on projects (projectID ASC);
+CREATE INDEX "projectTags" on tags (projectID ASC);
 CREATE UNIQUE INDEX "projectrating" ON projectratings(projectID ASC,userID ASC);
 CREATE UNIQUE INDEX "projectstat" ON projectstats(projectID ASC);
 
 CREATE UNIQUE INDEX "userid" on oauthusers (userid ASC);
-CREATE UNIQUE INDEX "oauthstring" on oauthusers (oauthstring ASC);
+
 CREATE UNIQUE INDEX "saveID" on projectversions (saveID ASC);
-CREATE UNIQUE INDEX "projectID" on projects (projectID ASC);
-CREATE INDEX "versions_projectID" on projectversions (projectID ASC);
 CREATE INDEX "date" on projectversions (date ASC);
 CREATE INDEX "projectDate" on projectversions (projectID ASC, date ASC);
 CREATE INDEX "tags_tag" on tags (tag ASC);
-CREATE INDEX "projectTags" on tags (projectID ASC);
 
 CREATE TABLE localusers(
 	"localuserID" INTEGER PRIMARY KEY,
