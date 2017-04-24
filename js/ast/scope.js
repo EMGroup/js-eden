cc97e4078898ba76068584fbbfc32d79661b4c9d
Nick Pope <nwpope@gmail.com>
2017-04-24 14:53:42 +0100

    Fix for generating undefined scope constructors

--- js/ast/scope.js
+++ js/ast/scope.js
@@ -624,7 +624,7 @@ Eden.AST.Scope.prototype._generate_loop_opti = function(ctx, options, rangeindex
 }
 
 Eden.AST.Scope.prototype.generate = function(ctx, scope, options) {
-	var constructor = this.generateConstructor(ctx,scope,options);
+	var constructor = (ctx) ? this.generateConstructor(ctx,scope,options) : "";
 	// Are we required to do a full optimisation? Some outer scope is needing it
 	if (options.fulllocal) {
 		//if (this.compiled) return this.compiled;
