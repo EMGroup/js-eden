8ad072377bbb58ad7328094c5b64a50d8f3db1f8
Nicolas Pope <nwpope@gmail.com>
2020-12-03 16:16:53 +0200

    Fix zindex of tooltip

--- js/core/eden.js
+++ js/core/eden.js
@@ -299,6 +299,14 @@
 		tooltip.style.top = y + "px";
 		tooltip.innerHTML = text;
 		tooltip.style.display = "block";
+
+		let dialogs = $('.ui-front');
+		let zindex = 0;
+		for (var i=0; i<dialogs.length; ++i) {
+			if (dialogs[i].style.zIndex > zindex) zindex = dialogs[i].style.zIndex;
+		}
+
+		tooltip.style.zIndex = zindex+1;
 	}
 	
 	EdenUI.closeTooltip = function () {
