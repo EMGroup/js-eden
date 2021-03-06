d79371b4b8127030bf375a84ad41ec52ee94392f
Nicolas Pope <nwpope@gmail.com>
2021-03-06 09:05:52 +0200

    Fix some package dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -262,7 +262,7 @@ module.exports = function (grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-uglify-es');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-gh-pages');
 
