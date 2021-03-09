275d4012daff43f162aceb7dd473c1aecf319c5f
Nicolas Pope <nwpope@gmail.com>
2021-03-09 19:17:38 +0200

    Fix for scoped when bug

--- js/ast/scopedscript.js
+++ js/ast/scopedscript.js
@@ -2,6 +2,7 @@ Eden.AST.ScopedScript = function(statements, scope) {
 	this.type = "scopedscript";
 	this.statements = statements;
 	this.scope = scope;
+	this.errors = [];
 }
 
 Eden.AST.ScopedScript.prototype.execute = function(ctx,base,scope,agent) {
