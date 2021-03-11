ee3cbece6a656a4838da31e1dbbb6e1ec2dde6c3
Nicolas Pope <nwpope@gmail.com>
2021-03-11 18:52:44 +0200

    Fix for html syntax highlight bug

--- js/ui/highlighter-rules.js
+++ js/ui/highlighter-rules.js
@@ -148,7 +148,7 @@ EdenUI.Highlight.prototype.START = function() {
 						this.pushMode();
 						this.mode = "SELECTOR";
 						break;
-	case "<"		:	if (this.cs3 && this.prevtoken != "OBSERVABLE" && this.prevtoken != ")" && this.prevtoken != "]" && this.prevtoken != "}") {
+	case "<"		:	if (this.cs3 && this.stream.tokenType(this.prevtoken) === 'operator') {
 							this.classes.push("htmltag");
 							this.pushMode();
 							this.mode = "HTML";
