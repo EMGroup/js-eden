7f19a5288e54d5af9df2525850994ff40eeb59b9
Nicolas Pope <nwpope@gmail.com>
2022-09-12 14:00:04 +0100

    Add json replacer

--- js/peer.js
+++ js/peer.js
@@ -245,7 +245,7 @@ Eden.Peer = function(master, id, password) {
 					}
 				}
                 if (!removed) {
-                    console.error("Remove failed", JSON.stringify(node.statements, " ", 4), obj.remove[i].id);
+                    console.error("Remove failed", JSON.stringify(node.statements, (key) => (key == "parent") ? undefined : key, " ", 4), obj.remove[i].id);
                 }
 			}
 			//console.log("Remove stat", stat);
