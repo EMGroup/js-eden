43e0b399289a362628e6f6664027b073b50b6136
Nicolas Pope <nwpope@gmail.com>
2022-09-12 13:51:26 +0100

    Add debug output

--- js/peer.js
+++ js/peer.js
@@ -245,7 +245,7 @@ Eden.Peer = function(master, id, password) {
 					}
 				}
                 if (!removed) {
-                    console.error("Remove failed");
+                    console.error("Remove failed", node.statements, obj.remove[i].id);
                 }
 			}
 			//console.log("Remove stat", stat);
